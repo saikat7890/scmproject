91e3175c6afa7c7e0ef36ed333c1d5494e77d69b
Saikat <saikat123@gmail.com>
2023-03-24 01:23:05 +0530

    Initial Commit

---
+++ Addition.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    int i,sum =0;
+    printf("Enter your number:");
+    scanf("%d",&i);
+
+    printf("The first %d natural number is:\n",i);
+    for(int j =1; j<=i; j++)
+    {
+        sum = sum +j;
+        printf("%d",j);
+    }
+    printf("\nThe Sum is : %d\n", sum);
+    return 0;
+}
